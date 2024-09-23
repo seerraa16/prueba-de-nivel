bc42a407d4ce500b7fba0fda7cd6e5cd45373483
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2024-09-23 11:38:11 +0200

    fin cambios

--- lanzador.py
+++ lanzador.py
@@ -6,32 +6,27 @@ from subsubclases.motocicletaa import Motocicleta
 def lanzador_main():
     vehiculos = []
 
-    mi_vehiculo = Coche("Rojo", 4, 180, 2000)
-    vehiculos.append(mi_vehiculo)
-    print(mi_vehiculo)
+    coche1= Coche("Naranja mandarina", 4, 140, 2000)
+    vehiculos.append(coche1)
+    print(coche1)
 
-    mi_vehiculo2 = Bicicleta("Azul", 2, "Urbana")
-    vehiculos.append(mi_vehiculo2)
-    print(mi_vehiculo2)
+    Bici1 = Bicicleta("Gris", 2, "Urbana")
+    vehiculos.append(Bici1)
+    print(Bici1)
 
-    mi_vehiculo3 = Camioneta("Rosa", 4, 2849, 160, 2500)
-    vehiculos.append(mi_vehiculo3)
-    print(mi_vehiculo3)
+    camioneta1 = Camioneta("Rosa", 4, 2849, 160, 2500)
+    vehiculos.append(camioneta1)
+    print(camioneta1)
 
-    mi_vehiculo4 = Bicicleta("Verde Brocoli", 2, "Deportiva")
-    vehiculos.append(mi_vehiculo4)
-    print(mi_vehiculo4)
-    mi_vehiculo5 = Camioneta( "Negro", 4, 1500, 140, 3000)
-    vehiculos.append(mi_vehiculo5)
-    print(mi_vehiculo5)
+    Bici2 = Bicicleta("Verde Brocoli", 2, "Deportiva")
+    vehiculos.append(Bici2)
+    print(Bici2)
 
-    mi_vehiculo6 = Motocicleta( "Blanca", 2, 220, 1200, "Deportiva")
-    vehiculos.append(mi_vehiculo6)
-    print(mi_vehiculo6)
+    Camioneta2 = Camioneta( "Negro", 4, 1500, 140, 3000)
+    vehiculos.append(Camioneta2)
+    print(Camioneta2)
 
-    mi_vehiculo4 = Motocicleta("Negra",2, 100, 1000, "Urbana")
-    vehiculos.append(mi_vehiculo4)
-    print(mi_vehiculo4)
+
 
     def catalogar(vehiculos, ruedas=None):
         if ruedas is not None:
